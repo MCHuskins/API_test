f19a89dd2d1f643f940f3dcf73170df19c3fa8ca
MCHuskins <mhuskins15@gmail.com>
2019-11-07 17:48:37 -0800

    I comible website and twiliosms and delatte unssary files

--- website.py
+++ website.py
@@ -1,15 +1,23 @@
 from flask import Flask
-import forecast as ds
+from twilio.rest import Client
+account_sid = 'ACc02a6cd029be8e49f8003dff669ba146'
+auth_token = 'bdee4244d1652feedc48987d5bcc87c9'
+#client is the progarme that talk to the twillio api
+client = Client(account_sid, auth_token)
+#stating flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    results = ds.get_forecast_data("41.619549", "-93.598022")
-    return results["summary"]
+    message = client.messages.create(
+                                  body='Guess how I send this -Matthew Huskins',
+                                  from_='+16502036811',
+                                  media_url=['https://media.tenor.com/images/4653cab601012d45914782bc482a6390/tenor.gif'],
+                                  to='+16506789378'
+                              )
 
-@app.route("/another/endpoint")
-def secret_hello():
-    return "You've found a secret endpoint!"
+    print(message.sid)
+    return("message sent")
 
 if __name__ == "__main__":
     app.run()
