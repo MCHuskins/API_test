0c14dcf3b34847f9ba968745eb7d3b1d5ba20636
David Fentz <bomjumaku@gmail.com>
2019-12-05 19:01:47 -0600

    changed some things up to get this to work.

--- website.py
+++ website.py
@@ -12,28 +12,46 @@ account_sid = os.environ["TWILLIO_ACCOUNT_SID"]
 # Your Auth Token from twilio.com/console
 auth_token  = os.environ["TWILLIO_AUTH_TOKEN"]
 
-#client is the progarme that talk to the twillio api
+#client is the program that talks to the twillio api
 client = Client(account_sid, auth_token)
-#stating flask
+#starting flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def my_form():
     return render_template('my-form.html')
 
-#@app.route('/', methods=['POST'])
-#def my_form_post():
-    text = request.form['text']
-    resever = request.form['resever']
+# @app.route('/', methods=['POST'])
+# def my_form_post():
+#     form_message = request.form['message']
+#     form_number = request.form['form_number']
+#     print(form_message)
+#     print(form_number)
+#     message = client.messages.create(
+#                                   body= form_message,
+#                                   from_='+17652272641',
+#                                   media_url=['https://media.tenor.com/images/4653cab601012d45914782bc482a6390/tenor.gif'],
+#                                   to= form_number
+#                               )
+
+#     print(message.sid)
+#     return "message sent"
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    form_message = request.form['msg']
+    form_number = request.form['target_num']
+    print(form_message)
+    print(form_number)
     message = client.messages.create(
-                                  body= text,
-                                  from_='+17652272641',
+                                  body= form_message,
+                                  from_='17652272641',
                                   media_url=['https://media.tenor.com/images/4653cab601012d45914782bc482a6390/tenor.gif'],
-                                  to= resever
+                                  to= form_number
                               )
 
     print(message.sid)
-    return "message sent"
+    return f"your message was was {request.form['msg']} and target number was {request.form['target_num']}"
 
 if __name__ == "__main__":
     app.run()
