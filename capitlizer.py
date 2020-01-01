582a477382b99e57b2a151cc5ed0519c055fcf9a
MCHuskins <mhuskins15@gmail.com>
2019-12-31 20:23:13 -0800

    test

--- capitlizer.py
+++ capitlizer.py
@@ -1,6 +1,8 @@
 import os
 import argparse
 
+#test
+
 parser = argparse.ArgumentParser(description='recursivley capitals all files in a directory.')
 parser.add_argument("target_directory", default=".")
 parser.add_argument("-l",  action="store_true")
